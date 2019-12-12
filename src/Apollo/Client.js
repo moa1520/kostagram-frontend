3108001f9ac1d806974f26203813ca31b202c60b
moa1520 <douit_be@naver.com>
2019-12-12 15:48:40 +0900

    fix url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -4,8 +4,8 @@ import { defaults, resolvers } from "./LocalState";
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "production"
-      ? "http://localhost:4000"
-      : "https://kostagram-backend.herokuapp.com/",
+      ? "https://kostagram-backend.herokuapp.com/"
+      : "http://localhost:4000",
   clientState: {
     defaults,
     resolvers

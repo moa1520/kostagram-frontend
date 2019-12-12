d63a75f28e2b7b986013a4a85780bc3b03b24cdb
moa1520 <douit_be@naver.com>
2019-12-12 15:44:47 +0900

    Real URL add

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "http://localhost:4000"
+      : "https://kostagram-backend.herokuapp.com/",
   clientState: {
     defaults,
     resolvers
